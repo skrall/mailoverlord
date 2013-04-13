3cdf39cac7de6c8fd2c0277ec8db36f0c5995ebb
Steve Krall <skrall@gmail.com>
2013-04-13 18:22:02 -0400

    Allow the JNDI name of the mail session to be overridden.

--- src/main/java/org/mailoverlord/server/config/JndiMailSessionConfig.java
+++ src/main/java/org/mailoverlord/server/config/JndiMailSessionConfig.java
@@ -2,9 +2,11 @@ package org.mailoverlord.server.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 import javax.mail.Session;
 import javax.naming.InitialContext;
@@ -18,12 +20,16 @@ public class JndiMailSessionConfig {
 
     private static final Logger logger = LoggerFactory.getLogger(JndiMailSessionConfig.class);
 
+    @Autowired
+    private Environment environment;
+
     @Bean
     public Session session() {
         Session session = null;
         try {
             InitialContext ic = new InitialContext();
-            session = (Session)ic.lookup("java:comp/env/mail/overlord-mail");
+            String mailSessionName = environment.getProperty("overlord-mail", "mail/overlord-mail");
+            session = (Session)ic.lookup(String.format("java:comp/env/%s", mailSessionName));
         } catch(Exception e) {
             logger.error("Error while looking up mail session.", e);
         }
