d6ec3f141c45602f4d72d5119d43cfe9f788db19
Steve Krall <skrall@gmail.com>
2013-04-28 08:35:01 -0400

    Update jackson to 2.2.0.

--- src/test/java/org/mailoverlord/server/config/TestConfig.java
+++ src/test/java/org/mailoverlord/server/config/TestConfig.java
@@ -1,6 +1,6 @@
 package org.mailoverlord.server.config;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
